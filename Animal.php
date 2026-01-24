1eed9ccfacf781fc85b95baf98e678c00691ec98
MikhaelLev <143705598+MikhaelLev@users.noreply.github.com>
2026-01-25 01:06:34 +0700

    Progress 24 Januari 2026

---
+++ Animal.php
@@ -0,0 +1,34 @@
+<?php
+namespace Data;
+
+require_once "Food.php";
+
+//dengan abstract function pada abstract class
+//kita dapat memaksa setiap child akan melaksanakan function dari abstract function yang ada
+abstract class Animal{
+    public string $name;
+
+    abstract public function run():void;
+
+    abstract public function eat(AnimalFood $animalFood): void;
+}
+
+class Cat extends Animal{
+    public function run():void{
+        echo "Cat $this->name is running" . PHP_EOL;
+    }
+
+    public function eat(AnimalFood $animalFood): void{
+        echo "Cat is eating" . PHP_EOL;
+    }
+}
+class Dog extends Animal{
+    public function run():void{
+        echo "Dog $this->name is running" . PHP_EOL;
+    }
+
+    public function eat(Food $animalFood): void{
+       echo "Dog is eating" . PHP_EOL;
+    }
+}
+?>
---
+++ PropertiesOverloading.php
@@ -0,0 +1,39 @@
+<?php
+class Zero{
+    private array $properties = [];
+
+    public function __get($name){
+        return $this->properties[$name];
+    }
+
+    public function __set($name, $value){
+        $this->properties[$name] = $value;
+    }
+
+    public function __isset($name):bool{
+        return isset($this->properties[$name]);
+    }
+    public function __unset($name){
+        unset($this->properties[$name]);
+    }
+
+    public function __call($name, $arguments){
+        $join = join(",", $arguments);
+        echo "Call function $name with arguments $join" . PHP_EOL;
+    }
+
+    public static function __callstatic($name, $arguments){
+        $join = join(",", $arguments);
+        echo "Call function $name with arguments $join" . PHP_EOL;
+    }
+}
+
+$zero = new Zero();
+$zero->firstName = "Eko";
+$zero->middleName = "Kurniawan";
+$zero->lastName = "Khannedy";
+
+$zero->sayHello("Eko", "Khannedy");
+Zero::sayHello("Eko", "Khannedy");
+
+?>
